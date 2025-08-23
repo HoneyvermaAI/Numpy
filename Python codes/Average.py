dcce280bd53ebd81f6cb522698d3534ebd27373a
Honey verma <honeyverma2k183632@gmail.com>
2025-08-23 14:45:43 +0530

    Numpy

--- Python codes/Average.py
+++
@@ -1,9 +0,0 @@
-from scipy.cluster.hierarchy import average
-
-temperatures = [32.8,36.2,33.4,33.9,28.6,29.4,10.2,8.3,45.0,8.2,22.6]
-total = 0
-for temp in temperatures:
-    total += temp
-average = total / len(temperatures)
-
-print("The average of the temperatures is : " , average)
