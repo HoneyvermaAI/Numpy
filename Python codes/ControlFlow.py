a419071195efeea776ab18fcdbd023aaf75c3620
Honey verma <honeyverma2k183632@gmail.com>
2025-09-06 23:13:02 +0530

    Numpy

--- Python codes/ControlFlow.py
+++ Python codes/ControlFlow.py
@@ -6,9 +6,28 @@
     Voting Eligibility Check.
 """
 
-Age = int(input("Enter your Age : "))
+# Age = int(input("Enter your Age : "))
 
-if (Age >= 18):
+"""if (Age >= 18):
     print("You are eligible to vote")
 else:
-    print("You are not eligible to vote")
+    print("You are not eligible to vote")"""
+
+
+
+"""
+Start , Stop and Skip range and Taking user input
+
+"""
+start = int(input("Enter the start Number : "))
+stop = int(input("Enter the Stop Number : "))
+skip = int(input("Enter the Number you want to skip : "))
+
+
+if start < stop and start < skip < stop:
+    for i in range(start, stop):
+        if i == skip:
+            continue
+        print(i)
+else:
+    print("The Number is Out of Range! Please check again.")
